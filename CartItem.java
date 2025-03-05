db05bbf5cf9c61ab0a21bfa29696816640f3b9a2
Ankit Yadav <ankit.yadav@metacube.com>
2025-03-05 14:28:16 +0530

    Remove repeated constructor

--- CartItem.java
+++ CartItem.java
@@ -4,14 +4,4 @@ record CartItem(
     String description,
     Float price,
     Integer quantity
-) {
-    
-    public CartItem(String id, String name, String description, Float price, Integer quantity) { 
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.price = price;
-        this.quantity = quantity;
-    }
-
-}
+) { }
